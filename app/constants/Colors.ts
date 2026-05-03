1ee29f96b93acebfd03fb63b40219d7591ccb06a
Hydley <hydley.silva@gmail.com>
2026-05-03 14:40:14 -0300

    feat: change dark mode background colro

--- app/constants/Colors.ts
+++ app/constants/Colors.ts
@@ -1,4 +1,4 @@
-const tintColorLight = '#2f95dc';
+const tintColorLight = '#dc662f';
 const tintColorDark = '#fff';
 
 export default {
@@ -11,7 +11,7 @@ export default {
   },
   dark: {
     text: '#fff',
-    background: '#000',
+    background: '#140c0f',
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
